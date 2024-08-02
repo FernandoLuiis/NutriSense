10325bb626a6438c859ef4cfc9aeef80d303f02c
FernandoLuiis <fpaludo2018@gmail.com>
2024-08-02 15:02:57 -0300

    Primeiro commit

---
+++ ButtonGradient.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function ButtonGradient({ onPress }) {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
+                <LinearGradient
+                    colors={['#000000', '#000000', '#000000']}
+                    start={{x: 1, y: 0}}
+                    end={{x: 0, y: 1}}
+                    style={styles.button}
+                >
+                    <Text style={styles.text}>Entrar</Text>
+                </LinearGradient>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        width: '80%', // Ajuste a largura conforme necessário
+    },
+    text: {
+        backgroundColor: 'transparent',
+        fontSize: 18,
+        color: '#fff',
+    },
+    button: {
+        height: 60,
+        borderRadius: 25,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 30,
+    },
+});
