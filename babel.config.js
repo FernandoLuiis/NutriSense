10ee85e83781c5d2a83e4ec7f1ef1710712d36cc
FernandoLuiis <fpaludo2018@gmail.com>
2024-08-07 20:52:18 -0300

    Ajustado esquema de pastas

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['metro-react-native-babel-preset'],
+    plugins: ['react-native-reanimated/plugin'], // Adicione esta linha
   };
 };
